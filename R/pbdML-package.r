982fbe3d0e80ebad4d5285cb73e1f31e87b667bc
wrathematics <wrathematics@gmail.com>
2015-09-12 21:08:06 -0400

    skeleton package help

---
+++ R/pbdML-package.r
@@ -0,0 +1,22 @@
+#' Machine Learning
+#' 
+#' TODO
+#' 
+#' \tabular{ll}{ 
+#'    Package: \tab pbdML \cr 
+#'    Type: \tab Package \cr 
+#'    License: \tab BSD 2-clause \cr 
+#'    LazyLoad: \tab yes \cr 
+#' } 
+#' 
+#' This package requires an MPI library (OpenMPI, MPICH2, or LAM/MPI).
+#' 
+#' @importFrom pbdMPI comm.stop
+#' @import pbdDMAT
+#' 
+#' @name pbdML-package
+#' @docType package
+#' @author Drew Schmidt \email{schmidt AT math.utk.edu}, George Ostrouchov, and Wei-Chen Chen.
+#' @references Programming with Big Data in R Website: \url{http://r-pbd.org/}
+#' @keywords Package
+NULL
