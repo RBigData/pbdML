d06a5d403f15e7158e778deffad5cba2e69bf77a
wrathematics <wrathematics@gmail.com>
2015-09-22 18:58:32 -0400

    fixed compile errors

--- src/any.c
+++ src/any.c
@@ -6,20 +6,21 @@
 // Check for NA, Inf, and NaN
 SEXP R_badVals(SEXP x_)
 {
+  SEXP ret;
   bool check =  false;
   double *x = REAL(x_);
   
-  for (int i=0; i<LENGTH(x); i++)
+  for (int i=0; i<LENGTH(x_); i++)
   {
     if (!R_FINITE(x[i]))
-      goto exit;
+      goto makeret;
   }
   
   check = true;
   
-  exit:
-  PROTECT(SEXP ret = allocVector(LGLSXP, 1));
-  INTEGER(ret)[0] = check;
+  makeret:
+    PROTECT(ret = allocVector(LGLSXP, 1));
+    INTEGER(ret)[0] = check;
   
   return ret;
 }
